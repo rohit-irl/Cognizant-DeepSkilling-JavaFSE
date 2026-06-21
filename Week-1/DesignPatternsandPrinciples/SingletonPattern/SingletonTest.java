025184a0c9e04a405261b1b8cac18796247fd5f1
rohit-irl <r.raj14112006@gmail.com>
2026-06-21 12:27:11 +0530

    Design-Patterns

---
+++ Week-1/DesignPatternsandPrinciples/SingletonPattern/SingletonTest.java
@@ -0,0 +1,18 @@
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("User Logged In");
+
+        if(logger1 == logger2) {
+            System.out.println("Both logger objects are same instance");
+        }
+        else {
+            System.out.println("Different instances");
+        }
+    }
+}
---
+++ Week-1/output.md
@@ -0,0 +1,9 @@
+## Execution Output
+
+### Singleton pattern
+
+![Singleton Output](./assets/singleton.png)
+
+### Factory Method pattern
+
+![Factory Output](./assets/factory.png)
